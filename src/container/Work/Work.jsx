b9940694937eeb224d02ddc45acbb025800b02f0
Begad <64358501+begadsalah@users.noreply.github.com>
2022-12-02 20:18:02 +0200

    work exp desc fix

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -7,14 +7,24 @@ import { urlFor, client } from "../../client";
 import "./Work.scss";
 
 const Work = () => {
-  const [activeFilter, setActiveFilter] = useState("All");
-  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
+  const [activeFilter, setActiveFilter] = useState("All");
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+
+  useEffect(() => {
+    const query = '*[_type == "works"]';
+
+    client.fetch(query).then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    });
+  }, []);
 
   const handleWorkFilter = (item) => {
     setActiveFilter(item);
     setAnimateCard([{ y: 100, opacity: 0 }]);
+
     setTimeout(() => {
       setAnimateCard([{ y: 0, opacity: 1 }]);
 
@@ -26,22 +36,14 @@ const Work = () => {
     }, 500);
   };
 
-  useEffect(() => {
-    const query = '*[_type == "works"]';
-    client.fetch(query).then((data) => {
-      setWorks(data);
-      setFilterWork(data);
-    });
-  }, []);
-
   return (
     <>
       <h2 className="head-text">
-        My Creative <span>Portfolio</span>
-        section
+        My Creative <span>Portfolio</span> Section
       </h2>
+
       <div className="app__work-filter">
-        {["UI/UX", "Web App", "Next JS", "React JS", "All"].map(
+        {["UI/UX", "Web App", "Mobile App", "React JS", "All"].map(
           (item, index) => (
             <div
               key={index}
@@ -55,51 +57,41 @@ const Work = () => {
           )
         )}
       </div>
+
       <motion.div
         animate={animateCard}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__work-portfolio"
       >
         {filterWork.map((work, index) => (
-          <div className="app__work-item app__flex">
+          <div className="app__work-item app__flex" key={index}>
             <div className="app__work-img app__flex">
               <img src={urlFor(work.imgUrl)} alt={work.name} />
+
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
                 transition={{
                   duration: 0.25,
                   ease: "easeInOut",
-                  staggerChilder: 0.5,
+                  staggerChildren: 0.5,
                 }}
                 className="app__work-hover app__flex"
               >
-                <a
-                  href={work.projectLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ opacity: [1, 0.9] }}
-                    transition={{
-                      duration: 0.25,
-                    }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
                     <AiFillEye />
                   </motion.div>
                 </a>
-                <a
-                  href={work.codeLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ opacity: [1, 0.9] }}
-                    transition={{
-                      duration: 0.25,
-                    }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
                     <AiFillGithub />
@@ -107,11 +99,13 @@ const Work = () => {
                 </a>
               </motion.div>
             </div>
+
             <div className="app__work-content app__flex">
               <h4 className="bold-text">{work.title}</h4>
               <p className="p-text" style={{ marginTop: 10 }}>
                 {work.description}
               </p>
+
               <div className="app__work-tag app__flex">
                 <p className="p-text">{work.tags[0]}</p>
               </div>
